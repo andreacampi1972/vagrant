8ff2cddf5042c61e130369440e7b6d8bc937c2e3
Andrea Campi <andrea.campi1972@gmail.com>
2023-01-26 14:12:52 +0100

    centralize windows script

--- vagrant-win2019/scripts/03_install_os_packages.ps1
+++ vagrant-win2019/scripts/03_install_os_packages.ps1
@@ -1,11 +1,1 @@
-echo "******************************************************************************"
-echo "Install custom packages " `date`
-echo "******************************************************************************"
-
-. sc.exe config winrm start= auto
-iwr -useb https://chocolatey.org/install.ps1 | iex
-choco install putty.install -y
-choco install windirstat -y
-choco install -y 7zip
-choco install -y googlechrome
-choco install -y notepadplusplus
+../../common_scripts/windows/03_install_os_packages.ps1
