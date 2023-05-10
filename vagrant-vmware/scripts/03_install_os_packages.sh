9deb99170fba48a1992bf019cedea92ff6cdc471
Andrea Campi <andrea.campi1972@gmail.com>
2023-05-10 20:23:59 +0200

    win11

--- vagrant-vmware/scripts/03_install_os_packages.sh
+++ vagrant-vmware/scripts/03_install_os_packages.sh
@@ -3,15 +3,7 @@ echo "Install custom packages " `date`
 echo "******************************************************************************"
 
 
-dnf install -y oraclelinux-developer-release-el8.x86_64
-dnf install -y python38-pip sshpass git-svn
-dnf install -y squid
-
-#Install vmware
-pip3.8 install requests pyVim PyVmomi pywinrm ansible oci-cli
-
-
-systemctl enable squid
-systemctl start squid
-
-shutdown 0
+echo "******************************************************************************"
+echo "IP  " `date`
+echo "******************************************************************************"
+ifconfig -a
--- vagrant-win2019/scripts/03_install_os_packages.ps1
+++ vagrant-win2019/scripts/03_install_os_packages.ps1
@@ -1,1 +1,12 @@
-../../common_scripts/windows/03_install_os_packages.ps1
+echo "******************************************************************************"
+echo "Install custom packages " `date`
+echo "******************************************************************************"
+
+. sc.exe config winrm start= auto
+iwr -useb https://chocolatey.org/install.ps1 | iex
+choco install putty.install -y
+choco install windirstat -y
+choco install -y 7zip.install
+choco install -y googlechrome
+choco install -y notepadplusplus
+choco install -y vscode.install
